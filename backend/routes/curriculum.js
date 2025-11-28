ee23a2093679894cf2ed5f9550a38b9c7f033f0d
juma5917 <jumasamwel422@gmail.com>
2025-11-28 17:29:53 +0300

    Remove .env file from repo

--- backend/routes/curriculum.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const curriculumController = require('../controllers/curriculumController');
-
-// GET /api/curriculum
-router.get('/', curriculumController.getCurriculum);
-
-module.exports = router;
